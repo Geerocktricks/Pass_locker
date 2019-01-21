ee3e543dfc7a5cfc16b60c1869e8db9b77fd1915
Geerocktricks <geerockface4@gmail.com>
2019-01-21 13:11:31 +0300

    This commit creates the User class

---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    '''
+    Class that generates new instances of users
+    '''
+    pass
+
+    def __init__(self,first_name , last_name):
+        '''
+        init method to create instances of the user class
+        
+        Args:
+            first_name: New User.first_name
+            last_name: New user.last_name
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
