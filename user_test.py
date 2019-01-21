8e06799d25e4282c09d7b9a39d2f28718340211a
Geerocktricks <geerockface4@gmail.com>
2019-01-21 14:15:54 +0300

    Creates new user test and adds ability to create new user

--- user_test.py
+++ user_test.py
@@ -17,3 +17,19 @@ class TestUser(unittest .TestCase):
         set up method to run before each test cases
         '''
         self.new_user = User('Gerald' , 'Muchuki')
+
+    #******************************tearDown method******************************************************
+
+                            #tearDown here
+
+    #*******************************Create user test******************************************************
+
+    def test_init(self):
+        '''
+        Test to see if users are instanciated properties
+        '''
+        self.assertEqual(self.new_user.first_name , 'Gerald')
+        self.assertEqual(self.new_user.last_name , 'Muchuki')
+
+if __name__ == '__main__':
+    unittest.main()
