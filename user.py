0f0a80150e9ff3e23e91b88c9a626c99f56fdf12
Geerocktricks <geerockface4@gmail.com>
2019-01-21 14:01:52 +0300

    This commit creates the TestUser class

--- user.py
+++ user.py
@@ -2,12 +2,12 @@ class User:
     '''
     Class that generates new instances of users
     '''
-    pass
-
+    user_list = [] #Empty user list
+    
     def __init__(self,first_name , last_name):
         '''
         init method to create instances of the user class
-        
+
         Args:
             first_name: New User.first_name
             last_name: New user.last_name
---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest
+from user import User
+
+class TestUser(unittest .TestCase):
+    '''
+    Tets case that defines test cases for the user class
+    behaviours
+    
+    Args:
+        unittest.TestUser: TestUser class that helps in creating test cases
+    '''
