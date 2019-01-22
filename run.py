f8fa4e10d155f795eba760c477356349e80a4572
Geerocktricks <geerockface4@gmail.com>
2019-01-22 15:14:07 +0300

    Adds the save user functionality

--- run.py
+++ run.py
@@ -1,6 +1,86 @@
 #!/usr/bin/env python3.6
 from user import User
 
+def create_user(first_name , last_name):
+    '''
+    Function to create new user
+    '''
+    new_user = User(first_name , last_name)
+    return new_user
+
+def save_users(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+print("\n")
+print("                                  ██████╗  █████╗ ███████╗███████╗    ██╗      ██████╗  ██████╗██╗  ██╗███████╗██████╗ ")
+print("                                  ██╔══██╗██╔══██╗██╔════╝██╔════╝    ██║     ██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗")
+print("                                  ██████╔╝███████║███████╗███████╗    ██║     ██║   ██║██║     █████╔╝ █████╗  ██████╔╝")
+print("                                  ██╔═══╝ ██╔══██║╚════██║╚════██║    ██║     ██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗")
+print("                                  ██║     ██║  ██║███████║███████║    ███████╗╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║")
+print("                                  ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝")
+print("                                 =====================================================================================\n")                                                                
+print("                                     ╦ ╦┌─┐┬  ┌─┐┌─┐┌┬┐┌─┐  ┌┬┐┌─┐  ╔═╗┌─┐┌─┐  ┌─┐┌─┐┌─┐┌─┐  ┬  ┌─┐┌─┐┬┌─┌─┐┬─┐┬┬")
+print("                                     ║║║├┤ │  │  │ ││││├┤    │ │ │  ║ ╦├┤ ├┤───├─┘├─┤└─┐└─┐  │  │ ││  ├┴┐├┤ ├┬┘││")
+print("                                     ╚╩╝└─┘┴─┘└─┘└─┘┴ ┴└─┘   ┴ └─┘  ╚═╝└─┘└─┘  ┴  ┴ ┴└─┘└─┘  ┴─┘└─┘└─┘┴ ┴└─┘┴└─oo\n")
+print("         _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+__+__+_+_+_+_+_+_+ ") 
+print('\n')                                  
+
+def main():
+    
+    print('                                                            HELLO , WELCOME TO PASS-LOCKER!. ')
+    print('                                                            =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=')
+    print('\n')
+    print('                                         This app will have access to your passwords. Would you like to proceed? [y/n]')
+    print('***********************************')
+    first_choice = input().lower()
+    valid = { "y": True,
+             "n": False}
+    print('                                  *')
+    print('                                  *')
+    print('                                  *')
+    print('                                  *')
+    if first_choice == 'y':
+        print('                                         [1].Login                            [2].Sign-up')
+        log_sign = input()
+        # valid = {
+        #     1:True,
+        #     2.False
+        # }
+        if log_sign == '2':
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print('                                         First name ....')
+            f_name = input().lower()
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print("                                         Last name .....")
+            l_name = input().lower()
+
+            save_users(create_user(f_name,l_name)) #Create and save new user
+            # print ('\n')
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print('                                  *')
+            print(f"                                        New user {f_name} {l_name} created")
+            print ('\n')
+    elif first_choice == 'n':
+        print('                                                                         Bye')
+    else:
+        print('                                         Invalid choice, Choose between (y = yes) and (n = no)')
+    print('                                  *')
+    print('                                  *')
+    print('                                  *')
+    print('                                  *')
+    
+
 if __name__ == '__main__':
 
     main()
+                            
